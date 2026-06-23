3b26d5fce38921e04f932fffa22b58c8f0e8ef42
fredericky123 <13982749+fredericky123@users.noreply.github.com>
2026-06-23 14:36:57 +0800

    Create namespace.ts

---
+++ lib/routes/cambridge/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Cambridge Core',
+    url: 'cambridge.org',
+    categories: ['journal'],
+    lang: 'en',
+};
