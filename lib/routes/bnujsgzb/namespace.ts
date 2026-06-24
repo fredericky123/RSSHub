1bf557f5341115125186f2fd38719c37007ea939
fredericky123 <13982749+fredericky123@users.noreply.github.com>
2026-06-24 21:50:49 +0800

    Create namespace.ts

---
+++ lib/routes/bnujsgzb/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '北京师范大学党委教师工作部',
+    url: 'jsgzb.bnu.edu.cn',
+    categories: ['university'],
+    lang: 'zh-CN',
+};
