dc8b1085bd0031b4d4485a19fded5c07490a1065
fredericky123 <13982749+fredericky123@users.noreply.github.com>
2026-06-24 22:40:07 +0800

    Create namespace.ts

---
+++ lib/routes/bnulib/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '北京师范大学图书馆',
+    url: 'libone.bnu.edu.cn',
+    categories: ['university'],
+    lang: 'zh-CN',
+};
