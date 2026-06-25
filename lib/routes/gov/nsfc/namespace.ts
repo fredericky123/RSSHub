9c2001a0f7c76c6710736306a88c0cf4ef46304c
fredericky123 <13982749+fredericky123@users.noreply.github.com>
2026-06-25 19:38:41 +0800

    Update namespace.ts

--- lib/routes/gov/nsfc/namespace.ts
+++ lib/routes/gov/nsfc/namespace.ts
@@ -2,7 +2,7 @@ import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
     name: '国家自然科学基金委员会',
-    url: 'www.nsfc.gov.cn',
+    url: 'nsfc.gov.cn',
     categories: ['government'],
     lang: 'zh-CN',
 };
