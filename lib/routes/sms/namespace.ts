ea82fd45856d15794471f639c50df1ed71c63693
fredericky123 <13982749+fredericky123@users.noreply.github.com>
2026-08-04 11:56:07 +0800

    Create namespace.ts

---
+++ lib/routes/sms/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Strategic Management Society',
+    url: 'strategicmanagement.net',
+    categories: ['journal'],
+    lang: 'en',
+};
